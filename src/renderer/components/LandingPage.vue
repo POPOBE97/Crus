31d5c94f2ef0504cfccd780fa5c727f831802043
ruiyao <luoruiyao@126.com>
2021-09-15 00:35:45 +0800

    Remove webpack-hot-middleware entry

--- src/renderer/components/LandingPage.vue
+++ src/renderer/components/LandingPage.vue
@@ -36,7 +36,10 @@
     padding: 0;
   }
 
-  body { font-family: 'Source Sans Pro', sans-serif; }
+  body {
+     font-family: 'Source Sans Pro', sans-serif; 
+     overflow: hidden;
+  }
 
   #wrapper {
     background:
--- src/renderer/components/StatusBar.vue
+++ src/renderer/components/StatusBar.vue
@@ -1,6 +1,12 @@
 <template>
   <v-system-bar height="30">
-    <p>Ruiyao's</p>
+    <v-container>
+      <v-row>
+          <p>Ruiyao</p>
+        <v-spacer></v-spacer>
+          <p>Ruiyao</p>
+      </v-row>
+    </v-container>
   </v-system-bar>
 </template>
 
